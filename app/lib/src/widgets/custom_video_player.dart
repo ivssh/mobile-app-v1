7f28826bfd113589ad0bc1705be20465b02fe8dc
Abhilash Bolla <abhilash@mello.fun>
2023-01-20 18:35:55 +0530

    Added custom video player widget

---
+++ app/lib/src/widgets/custom_video_player.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:shared/modules/posts/models/posts.dart';
+import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+
+
+class CustomVideoPlayer extends StatefulWidget {
+  final Post post;
+
+  const CustomVideoPlayer({
+    required this.post,
+  });
+
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    controller = VideoPlayerController.asset(widget.post.assetPath);
+    controller.initialize().then((_) {
+      setState(() {});
+    });
+    
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          if (controller.value.isPlaying) {
+            controller.pause();
+          } else {
+            controller.play();
+          }
+        });
+      },
+      child: AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: Stack(
+          children: [
+            VideoPlayer(controller),
+            const Positioned.fill(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black,
+                      Colors.transparent,
+                      Colors.transparent,
+                      Colors.black
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.0, 0.2, 0.8, 1.0],
+                  )
+                )
+              )
+            )
+          ],
+        ),
+      ),
+    ); 
+  }
+}
